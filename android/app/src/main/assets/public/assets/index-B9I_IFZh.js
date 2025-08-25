0692ec1ba8cfd527ab40dadd8aec8d305ba0570d
Your Name <your.email@example.com>
2025-08-25 15:42:11 +0200

    Build version 100.200 with correct signing key - Ready for Google Play deployment

---
+++ android/app/src/main/assets/public/assets/index-B9I_IFZh.js
@@ -0,0 +1,3 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-keXVRFGT.js","./index-D6Zlv4TB.js","./ui-4JzjGctU.js","./vendor-D63tyEUO.js","./supabase-DmvLFwnu.js","./index-Bg-672oM.css"])))=>i.map(i=>d[i]);
+import{_ as p}from"./supabase-DmvLFwnu.js";import{a4 as t}from"./index-D6Zlv4TB.js";import"./vendor-D63tyEUO.js";import"./ui-4JzjGctU.js";const _=t("App",{web:()=>p(()=>import("./web-keXVRFGT.js"),__vite__mapDeps([0,1,2,3,4,5]),import.meta.url).then(r=>new r.AppWeb)});export{_ as App};
+//# sourceMappingURL=index-B9I_IFZh.js.map
---
+++ android/app/src/main/assets/public/assets/test-tube-DyC9_ZKE.js
@@ -0,0 +1,7 @@
+import{o as e}from"./index-D6Zlv4TB.js";/**
+ * @license lucide-react v0.526.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M14.5 2v17.5c0 1.4-1.1 2.5-2.5 2.5c-1.4 0-2.5-1.1-2.5-2.5V2",key:"125lnx"}],["path",{d:"M8.5 2h7",key:"csnxdl"}],["path",{d:"M14.5 16h-5",key:"1ox875"}]],c=e("test-tube",t);export{c as T};
+//# sourceMappingURL=test-tube-DyC9_ZKE.js.map
